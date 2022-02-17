ba3aec97215525b3031d9675d21fd10758214384
srilokanas <67470659+srilokanas@users.noreply.github.com>
2022-02-17 21:56:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ async function display(){
 
     // q = Popular Videos --> Popular%20Videos (%20 -> Space)
 
-    let res = await fetch(`https://youtube.googleapis.com/youtube/v3/search?q=popular%20videos&key=AIzaSyA00OxUbfdfVAjHn-9l48s0t-n_j7CBbNs&maxResults=25`);
+    let res = await fetch(`https://youtube.googleapis.com/youtube/v3/search?q=popular%20videos&key=AIzaSyC1xlvVqSJK_c7fF1fHNFIBIJIhlPfpCZA&maxResults=25`);
 
     let data = await res.json();
     
@@ -60,7 +60,7 @@ async function searchVideos(){
     let query = document.getElementById("video").value; // Search String/ Query String
 
 //search for videos
-let res = await fetch(`https://youtube.googleapis.com/youtube/v3/search?q=${query}&type=video&key=AIzaSyA00OxUbfdfVAjHn-9l48s0t-n_j7CBbNs&maxResults=25`);
+let res = await fetch(`https://youtube.googleapis.com/youtube/v3/search?q=${query}&type=video&key=AIzaSyC1xlvVqSJK_c7fF1fHNFIBIJIhlPfpCZA&maxResults=25`);
 let data = await res.json();
 
 for({id:{videoId}}of data.items)
